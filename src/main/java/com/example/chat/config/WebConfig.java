95f24e21cda2b8331ca3da154404d49fd90d4259
LeeGaHyun12 <89252433+LeeGaHyun12@users.noreply.github.com>
2024-08-30 14:36:22 +0900

    Update WebConfig.java

--- src/main/java/com/example/chat/config/WebConfig.java
+++ src/main/java/com/example/chat/config/WebConfig.java
@@ -11,10 +11,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("https://api.icebuckwheat.kro.kr")
+                .allowedOrigins("https://api.icebuckwheat.kro.kr","http://localhost:8080")
                 .allowedMethods("*")
                 .allowedHeaders("*")
+                .allowCredentials(true);
+        
+        // WebSocket에 대한 CORS 설정 추가
+        registry.addMapping("/ws/**")
+                .allowedOrigins("https://api.icebuckwheat.kro.kr", "http://localhost:8080")
+                .allowedMethods("GET", "POST")
                 .allowCredentials(true);
     }
 }
