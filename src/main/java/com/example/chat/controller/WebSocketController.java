6f45ac8066e212257c60fd3488a69d1309e5caa0
LeeGaHyun12 <89252433+LeeGaHyun12@users.noreply.github.com>
2024-08-30 12:01:18 +0900

    Update WebSocketController.java

--- src/main/java/com/example/chat/controller/WebSocketController.java
+++ src/main/java/com/example/chat/controller/WebSocketController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class WebSocketController {
 
     @Autowired
